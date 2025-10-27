895f70c18dd5a950c5cc4cfecabceff05d978533
NeonTheNovaDesu <NeonTheNova82@gmail.com>
2025-10-27 23:52:08 +0330

    add task4 c# version

---
+++ Task4/Task4.2/Task4.2/Task4.2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[,,] arr = {
+    { {"sara" },{"computer"},{"math"}  },
+	{ { "ali"},{ "math"},{"law"} }, 
+	{ { "reza"},{ "computer"},{ "law"} }
+};
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        for (int k = 0; k < arr.GetLength(2); k++)
+        Console.Write(arr[i, j, k]);
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+}
+
